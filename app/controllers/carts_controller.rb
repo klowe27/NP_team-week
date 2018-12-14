f5a8714d1baaee21e05b671432145e375e8dc6e1
Student <example@example.com>
2018-12-13 18:40:56 -0800

    add stripe to cart views

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,4 +3,29 @@ class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
   end
+
+  def new
+      @order_items = current_order.order_items
+  end
+
+  def create
+  # Amount in cents
+  @amount = current_order.calculate_total_cent
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+  )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
 end
