8ab2d7313fd97bd80645b2c07ac0fc2e4a895108
Student <example@example.com>
2018-12-19 09:43:29 -0800

    fixed search?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
     if params[:search]
-    @nonprofit = Nonprofit.where('name LIKE ?', "%#{search}%" )
+    @nonprofit = Nonprofit.where('name LIKE ?', "%#{params[:search]}%" )
+  else
+    @nonprofit = Nonprofit.all
     end
   end
 
